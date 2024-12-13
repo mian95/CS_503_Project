99de861df83f0f2c8113c6df9fdb7f261902e8a0
mian95 <umairim24@gmail.com>
2024-12-13 09:32:32 +0500

    add the responsive navbar

---
+++ index.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active'); // Toggle the active class
+    openBtn.style.display = navLinks.classList.contains('active') ? 'none' : 'block'; // Show/hide open button
+    closeBtn.style.display = navLinks.classList.contains('active') ? 'block' : 'none'; // Show/hide close button
+});
